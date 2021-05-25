358175c9a6db09f17474e5b3ff203a768b8804cd
Anoromi <72703146+Anoromi@users.noreply.github.com>
2021-05-25 17:15:48 +0300

    Peck upd 4

--- src/App.java
+++ src/App.java
@@ -9,12 +9,11 @@ public class App {
         JFrame f = new JFrame();
         f.setLayout(new BorderLayout());
         Game g = new Game();
-        g.start();
-
         f.add(g.getCanvas());
+        f.setVisible(true);
+        g.start();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(1000, 1000);
-        f.setVisible(true);
 
     }
 }
